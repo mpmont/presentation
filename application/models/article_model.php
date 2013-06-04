496784229f474ef199fa597de2ffaceff532e0bd
Marco Monteiro <marco@marcomonteiro.net>
2013-06-04 18:54:04 +0100

    Removed all the spaces! also added a model...

---
+++ application/models/article_model.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Articles_Model extends CI_Model {
+
+
+
+}
+
+/* End of file articles_Model.php */
+/* Location: ./application/models/articles_Model.php */
